7b4e24dbf1316b0c0961bac482527b999a32add6
Eugene <68537036+eugene1488-png@users.noreply.github.com>
2023-03-28 16:42:46 +0700

    main
    форма + слайдер

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const block1 = document.getElementById("block1");
 const block2 = document.getElementById("block2");
 const block3 = document.getElementById("block3");
@@ -101,6 +102,156 @@ block8.addEventListener("mouseout", () => {
   blockimg1.style.display = "inline-block";
   blockimg8.style.display = "none";
 });
+
+
+
+*/
+
+
+
+
+
+
+
+const blocks = document.querySelectorAll(".page-three_block_text_hover");
+const blockImgs = document.querySelectorAll(".blockimg");
+let lastDisplayedImg = null;
+
+blocks.forEach((block) => {
+  const blockImgId = block.dataset.img;
+  const blockImg = document.getElementById(blockImgId);
+  
+  block.addEventListener("mouseover", () => {
+    blockImgs.forEach((img) => {
+      img.style.display = "none";
+    });
+    blockImg.style.display = "inline-block";
+    lastDisplayedImg = blockImg;
+  });
+
+  block.addEventListener("mouseout", () => {
+    blockImgs.forEach((img) => {
+      if (img !== lastDisplayedImg) {
+        img.style.display = "none";
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let offset = 0; 
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.next').addEventListener('click', function(){
+  offset = offset + 243;
+  sliderLine.style.left = -offset + 'px';
+
+  if (offset > 972) {
+    offset = 0;
+    sliderLine.style.left = 0;
+  }
+
+});
+
+/*
+document.querySelector ('.prev'). addEventListener('click', function(){
+  offset = offset - 243; 
+  sliderLine.style.left = offset + 'px';
+
+  if (offset < 0) {
+    
+    offset = 0;
+    sliderLine.style.left = 0;
+
+
+    
+  }
+  
+});
+
+*/
+
+
+document.querySelector('.prev').addEventListener('click', function() {
+  offset -= 243;
+  if (offset < 0) {
+    offset = 972; // устанавливаем значение, соответствующее последнему слайду
+    sliderLine.style.left = -offset + 'px';
+  } else {
+    sliderLine.style.left = -offset + 'px';
+  }
+});
+
+
+
+
+
+
+$(document).ready(function() {
+  // Массив с пятью текстовыми строками
+  var texts = [
+    "Aistein Bullet Camera",
+    "Aistein PTZ Dome Camera",
+    "Aistein Dome Camera",
+    "Aistein BRIDGE",
+    "Aistein RADAR"
+  ];
+
+  // Находим элементы на странице
+  var block = $('#my-block');
+  var prevButton = $('.prev');
+  var nextButton = $('.next');
+
+  // Счетчик текущего текста в массиве
+  var currentIndex = 0;
+
+  // Меняем текст в блоке на первый элемент массива
+  block.text(texts[currentIndex]);
+
+  // Привязываем обработчик события нажатия на кнопку "Назад"
+  prevButton.click(function() {
+    // Выбираем предыдущий элемент массива
+    var prevIndex = (currentIndex - 1 + texts.length) % texts.length;
+    var prevText = texts[prevIndex];
+
+    // Меняем текст в блоке
+    block.text(prevText);
+
+    // Обновляем счетчик текущего текста
+    currentIndex = prevIndex;
+  });
+
+  // Привязываем обработчик события нажатия на кнопку "Вперед"
+  nextButton.click(function() {
+    // Выбираем следующий элемент массива
+    var nextIndex = (currentIndex + 1) % texts.length;
+    var nextText = texts[nextIndex];
+
+    // Меняем текст в блоке
+    block.text(nextText);
+
+    // Обновляем счетчик текущего текста
+    currentIndex = nextIndex;
+  });
+});
+
 
 
 
